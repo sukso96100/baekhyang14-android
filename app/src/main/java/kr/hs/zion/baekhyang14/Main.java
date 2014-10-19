46b535267dd1a6111373eee6877440b892d4fa74
sukso96100 <sukso96100@gmail.com>
2014-10-19 23:16:52 +0900

    update

--- app/src/main/java/kr/hs/zion/baekhyang14/Main.java
+++ app/src/main/java/kr/hs/zion/baekhyang14/Main.java
@@ -8,10 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.ScrollView;
-
-import com.manuelpeinado.fadingactionbar.FadingActionBarHelper;
 
 
 public class Main extends ActionBarActivity {
